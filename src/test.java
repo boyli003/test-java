b51d547984ae24d3153e9eae5703a20845ee5c95
lizhangjun10 <lichangjun1@jd.com>
2016-09-07 18:26:42 +0800

    no message

--- src/test.java
+++ src/test.java
@@ -5,5 +5,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("my is from_master_1");
         System.out.println("my is from_master_2");
+        System.out.println("my is from_master_2");
+        System.out.println("my is master");
     }
 }
